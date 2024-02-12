8d5464cf8a71f8eb0f79aaeaccb95e5c5dcdb318
blackcodding <supriyabarai77@gmail.com>
2024-02-13 02:27:55 +0530

    Setup react navigation and screen

---
+++ src/screens/OnboardingScreen/OnboardingScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export const OnboardingScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>{'OnboardingScreen'}</Text>
+    </SafeAreaView>
+  )
+}
