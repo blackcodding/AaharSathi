e76578d26196c24b4eae387bec98a42e91255756
blackcodding <supriyabarai77@gmail.com>
2024-03-31 14:56:00 +0530

    add: Added Upcoming list card

--- src/components/Cards/ListCard/ListCard.styles.ts
+++ src/components/Cards/ListCard/ListCard.styles.ts
@@ -5,24 +5,26 @@ export const generateStyles = () => {
   return StyleSheet.create({
     mainContainer: {
       width: 250,
-      height: 100,
-      backgroundColor: DEFAULT_COLOR.WHITE,
-      borderWidth: 1.5,
-      borderRadius: 12,
-      borderColor: DEFAULT_COLOR.BLACK,
+      flexDirection: 'column',
+      justifyContent: 'space-between',
       marginTop: 8,
       marginRight: 8,
       padding: 10,
+      borderWidth: 1.5,
+      borderRadius: 12,
+      borderColor: DEFAULT_COLOR.BLACK,
+      backgroundColor: DEFAULT_COLOR.WHITE,
     },
     headerContainer: {
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'flex-start',
     },
-    upComingListNameContainer: {
+    headingContainer: {
       flexDirection: 'row',
     },
-    upComingListName: {
+    heading: {
+      fontSize: 16,
       fontFamily: 'Rubik-Bold',
       color: DEFAULT_COLOR.BLACK,
       marginRight: 8,
@@ -37,6 +39,82 @@ export const generateStyles = () => {
       borderRadius: 50,
       backgroundColor: DEFAULT_COLOR.BLACK,
       marginRight: 2,
+    },
+    subheading: {
+      fontSize: 12,
+      fontFamily: 'Rubik-Bold',
+      color: DEFAULT_COLOR.BLACK,
+      marginTop: 2,
+    },
+    iconContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: 2,
+    },
+    imageContainer: {
+      height: 30,
+      width: 30,
+      borderRadius: 50,
+    },
+    image: {
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+      borderRadius: 50,
+    },
+    arrowContainer: {
+      height: 30,
+      width: 30,
+      alignSelf: 'flex-end',
+      borderWidth: 1,
+      borderRadius: 50,
+      borderColor: DEFAULT_COLOR.BLACK,
+      opacity: 0.1,
+    },
+    commonImage: {
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+    },
+    detailContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: 18,
+    },
+    dateContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    calenderContainer: {
+      height: 15,
+      width: 15,
+      opacity: 0.6,
+    },
+    date: {
+      fontSize: 10,
+      fontFamily: 'Rubik-Medium',
+      color: DEFAULT_COLOR.BLACK,
+      marginLeft: 6,
+    },
+    amountContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    amount: {
+      fontSize: 10,
+      fontFamily: 'Rubik-Medium',
+      color: DEFAULT_COLOR.BLACK,
+    },
+    addItemContainer: {
+      height: 20,
+      width: 20,
+      borderRadius: 50,
+      marginLeft: 6,
+      opacity: 0.6,
     },
   });
 };
--- src/components/Cards/ListCard/ListCard.tsx
+++ src/components/Cards/ListCard/ListCard.tsx
@@ -1,7 +1,8 @@
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 
 import {DEFAULT_COLOR} from '../../../Theme/Theme';
 import {IListCardProps} from './ListCard.types';
+import {Profile} from '../../Profile/Profile';
 import React from 'react';
 import {Tag} from '../../Tags/Tag';
 import {generateStyles} from './ListCard.styles';
@@ -13,8 +14,8 @@ export const ListCard = (props: IListCardProps) => {
   return (
     <View style={styles.mainContainer}>
       <View style={styles.headerContainer}>
-        <View style={styles.upComingListNameContainer}>
-          <Text style={styles.upComingListName}>{'Birthday List'}</Text>
+        <View style={styles.headingContainer}>
+          <Text style={styles.heading}>{'Birthday List'}</Text>
           <Tag
             text={'5 Items'}
             color={DEFAULT_COLOR.WHITE}
@@ -25,6 +26,49 @@ export const ListCard = (props: IListCardProps) => {
           <View style={styles.bullet} />
           <View style={styles.bullet} />
           <View style={styles.bullet} />
+        </View>
+      </View>
+      <Text style={styles.subheading}>{'3 Item remaining'}</Text>
+      <View style={styles.iconContainer}>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={{
+              uri: 'https://ik.imagekit.io/s1qqeedcv/AaharSathi/Onboarding%20Screen.png?updatedAt=1707764562848',
+            }}
+          />
+        </View>
+        <View style={styles.arrowContainer}>
+          <Image
+            style={styles.commonImage}
+            source={{
+              uri: 'https://ik.imagekit.io/s1qqeedcv/AaharSathi/next.png?updatedAt=1711578556017',
+            }}
+          />
+        </View>
+      </View>
+      <View style={styles.detailContainer}>
+        <View style={styles.dateContainer}>
+          <View style={styles.calenderContainer}>
+            <Image
+              style={styles.commonImage}
+              source={{
+                uri: 'https://ik.imagekit.io/s1qqeedcv/AaharSathi/calendar.png?updatedAt=1711578507237',
+              }}
+            />
+          </View>
+          <Text style={styles.date}>{'1st April 2024'}</Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amount}>{'$: 500'}</Text>
+          <View style={styles.addItemContainer}>
+            <Image
+              style={styles.commonImage}
+              source={{
+                uri: 'https://ik.imagekit.io/s1qqeedcv/AaharSathi/plus.png?updatedAt=1711579118614',
+              }}
+            />
+          </View>
         </View>
       </View>
     </View>
