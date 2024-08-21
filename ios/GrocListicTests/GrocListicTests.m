f46c8cf4b572244ffdefc611bbefb4ced5691e4a
deepakSenseHawk <deepakb@sensehawk.com>
2024-08-22 04:03:53 +0530

    chore: Changed App Name

--- ios/GrocListicTests/GrocListicTests.m
+++ ios/GrocListicTests/GrocListicTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AaharSathiTests : XCTestCase
+@interface GrocListicTests : XCTestCase
 
 @end
 
-@implementation AaharSathiTests
+@implementation GrocListicTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
