91bd23d6dc89349fcb21d78202668b42c0ac72e9
blackcodding <supriyabarai77@gmail.com>
2024-12-01 02:01:23 +0530

    add: Added Logout Icon

---
+++ src/assets/icons/LogoutIcon.tsx
@@ -0,0 +1,28 @@
+import {Path, Svg} from 'react-native-svg';
+
+import {DEFAULT_COLOR} from '../../Theme/Theme';
+import {IIconProps} from './icon.types';
+import React from 'react';
+import {View} from 'react-native';
+
+export const LogoutIcon = (props: IIconProps) => {
+  const {
+    width = 28,
+    height = 28,
+    fillColor = DEFAULT_COLOR.WHITE,
+    strokeWidth = 2,
+    strokeColor = DEFAULT_COLOR.BLUE_DARK,
+  } = props;
+
+  return (
+    <Svg width={width} height={height} viewBox={'0 0 24 24'} fill={fillColor}>
+      <Path
+        d="M16 17L21 12M21 12L16 7M21 12H9M9 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21H9"
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap={'round'}
+        strokeLinejoin={'round'}
+      />
+    </Svg>
+  );
+};
